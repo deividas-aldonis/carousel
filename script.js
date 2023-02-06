7bcb8609cf9d3aa7a157e1cac3b73c29d7397628
Deividas <deividas.aldonis.dev@gmail.com>
2023-02-06 17:43:15 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+const carouselBullets = document.querySelectorAll(".carousel-bullets .bullet");
+const activeBullet = document.querySelector(".carousel-bullets .active");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+let counter = 1;
+let bullet = 0;
+const size = carouselImages[0].clientWidth;
+const bulletSize = 20;
+
+carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+activeBullet.style.transform = "translateX(" + bulletSize + "px)";
+
+nextBtn.addEventListener("click", () => {
+  if (counter >= carouselImages.length - 1) return;
+  carouselSlide.style.transition = "transform .4s ease-in-out";
+  counter++;
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+});
+
+prevBtn.addEventListener("click", () => {
+  if (counter <= 0) return;
+  carouselSlide.style.transition = "transform .4s ease-in-out";
+  counter--;
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+});
+
+carouselSlide.addEventListener("transitionend", () => {
+  if (carouselImages[counter].classList.contains("lastClone")) {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - 2;
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  }
+
+  if (carouselImages[counter].classList.contains("firstClone")) {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  }
+
+  activeBullet.style.transform = "translateX(" + counter * bulletSize + "px)";
+});
+
+carouselBullets.forEach((bullet, index) => {
+  carouselSlide.style.transition = "transform .4s ease-in-out";
+
+  bullet.addEventListener("click", (b) => {
+    counter = parseInt(b.target.dataset.id);
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  });
+});
